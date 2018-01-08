e7da53947ffadd48549398efa83406a096943ed8
Kyle Firmstone <kfirmstonecpt@gmail.com>
2018-01-08 05:16:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(){
+  a+b
+}
+function subtract(){
+  a-b
+}
+function multiply(){
+  a*b
+}
+function divide(){
+  a/b
+}
+function inc(n){
+  n+1
+}
+function dec(n){
+  n-1
+}
+function makeInt(n){
+  parseInt(n,10)
+}
+function preserveDecimal(n){
+  parseFloat(n,10)
+}
+}
+}
