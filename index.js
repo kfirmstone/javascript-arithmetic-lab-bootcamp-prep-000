a7a8eccafd81e1bc979dc8260099b0673ed2150a
Kyle Firmstone <kfirmstonecpt@gmail.com>
2018-01-08 05:38:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ function divide(a,b){
   return a/b
 }
 function inc(n){
-  n++
+  return n+1
 }
 function dec(n){
-  n-1
+ return n-1
 }
 function makeInt(n){
-  parseInt(n,10)
+  return parseInt(n,10)
 }
 function preserveDecimal(n){
-  parseFloat(n,10)
+ return parseFloat(n,10)
 }
